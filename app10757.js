17c60c7153064fa1b000ad4f2967abea79773e63
cho_jae_sin <1005jsc@naver.com>
2022-05-22 23:36:41 +0900

    10757

---
+++ app10757.js
@@ -0,0 +1,13 @@
+let input = require('fs').readFileSync('input10757.txt').toString()
+
+
+
+input = input.split(' ')
+
+
+const A = BigInt(input[0]);
+const B = BigInt(input[1]);
+let answer = A + B;
+console.log(answer.toString());
+
+
---
+++ app2839.js
@@ -0,0 +1,13 @@
+let input = require('fs').readFileSync('input2839.txt').toString()
+
+let N = input
+
+
+
+
+let k = Math.floor(N/5)
+if(k%3!==0){
+
+}
+
+console.log(k)
