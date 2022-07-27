45f65c2890a956e00a6780fb00ffbefd22c72343
cho_jae_sin <1005jsc@naver.com>
2022-07-27 22:49:17 +0900

    1

---
+++ app2798.js
@@ -0,0 +1,5 @@
+let input = require('fs').readFileSync('input2798.txt').toString();
+
+input = input.split(' ');
+
+console.log(input);
---
+++ 맞춘 문제/app10872.js
@@ -0,0 +1,16 @@
+let input = require('fs').readFileSync('input10872.txt').toString()
+
+
+
+let b = 1
+for(let i= 1; i<=input; i++){
+
+    b= b*i
+    
+}
+
+console.log(b)
+
+
+
+
